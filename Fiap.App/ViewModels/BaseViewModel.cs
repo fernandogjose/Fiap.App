cb3dab74ae9d8ace4b9e80d2fd37c8c4800fcf02
Logon Aluno <logonaluno@local.com>
2020-03-04 20:53:59 -0300

    ajuste na base

--- Fiap.App/ViewModels/BaseViewModel.cs
+++ Fiap.App/ViewModels/BaseViewModel.cs
@@ -8,35 +8,41 @@ namespace Fiap.App.ViewModels
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
+        public BaseViewModel()
+        {
+
+        }
+
+        string title;
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (title == value) return;
+
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        bool isBusy;
+        public bool IsBusy
+        {
+            get => isBusy;
+            set
+            {
+                if (isBusy == value) return;
+
+                isBusy = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IsNotBusy));
+            }
+        }
+
+        public bool IsNotBusy => !IsBusy;
+
         public event PropertyChangedEventHandler PropertyChanged;
-    }
-
-    public class MonkeyViewModel : BaseViewModel
-    {
-        HttpClient httpClient;
-
-        HttpClient HttpClient => httpClient ?? (httpClient = new HttpClient());
-
-        public ObservableCollection<Monkey> Monkeys { get; }
-
-        public MonkeyViewModel()
-        {
-            Title = "Monkey Finder";
-            Monkeys = new ObservableCollection<Monkey>();
-        }
-
-        async Task GetMonkeysAsync()
-        {
-            try
-            {
-                var json = await HttpClient.GetStringAsync("");
-                var monkeys = Monkey.From
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
-        }
+        public void OnPropertyChanged([CallerMemberName]string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
