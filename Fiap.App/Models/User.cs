580eca333d7dd190a1b1d5f1f12ab66f0a8a28a0
Logon Aluno <logonaluno@local.com>
2020-03-04 22:25:44 -0300

    ajuste no nome do model

--- Fiap.App/Models/User.cs
+++ Fiap.App/Models/User.cs
@@ -2,10 +2,13 @@
 {
     public class User
     {
+        [JsonProperty("name")]
         public string Name { get; set; }
 
+        [JsonProperty("email")]
         public string Email { get; set; }
 
+        [JsonProperty("status")]
         public string Status { get; set; }
     }
 }
